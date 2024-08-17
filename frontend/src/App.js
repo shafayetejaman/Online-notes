595df962d666d805eb30dc53ae66d0662dce7c70
shafayetejaman <shafayetejaman@gmail.com>
2024-08-17 17:32:36 +0600

    backend auth added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-console.log(process.env.REACT_APP_KEY)
-
 function App()
 {
   return (
     <div className="App">
-      <h1>{process.env.KEY}</h1>
+      <h1>{process.env.REACT_APP_KEY}</h1>
     </div>
   );
 }

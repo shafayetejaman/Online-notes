03d53cc420872603fc7536ce208817f791b8bb56
shafayetejaman <shafayetejaman@gmail.com>
2024-09-23 21:21:41 +0600

    initial setup complete

--- start.sh
+++
@@ -1,2 +0,0 @@
-python3 backend/manage.py makemigrations && python3 backend/manage.py migrate;
-concurrently "python3 backend/manage.py runserver localhost:8000" "npm start --prefix frontend localhost:3000"
